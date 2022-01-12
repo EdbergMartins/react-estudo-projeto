150b93218eb31ffa3fa2e8bc1bbc7c30647c2893
EdberMartins <edbergmartins@gmail.com>
2022-01-11 21:12:04 -0300

    inicialização de paginas e aplicação de css basico

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewPorject from "./components/pages/NewPorject";
+
+import Container from "./components/layout/Container";
+
 function App() {
   return (
     <div className="App">
-      <p>Inicio do projeto</p>
+      <Router>
+        <div>
+          <Link to="/">Home </Link>
+          <Link to="/company">Contato </Link>
+          <Link to="/contact">Empresa </Link>
+          <Link to="/newproject">Novo Projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/company" element={<Company />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/newproject" element={<NewPorject />} />
+          </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
