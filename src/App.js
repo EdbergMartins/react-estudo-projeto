2e505bfac8790b59f3977ff8ed046698f55026e5
EdberMartins <edbergmartins@gmail.com>
2022-01-12 15:57:48 -0300

    Adição do Navbar sem css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import NewPorject from "./components/pages/NewPorject";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Link to="/">Home </Link>
-          <Link to="/company">Contato </Link>
-          <Link to="/contact">Empresa </Link>
-          <Link to="/newproject">Novo Projeto</Link>
-        </div>
+        <Navbar/>
         <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -24,7 +21,7 @@ function App() {
             <Route exact path="/newproject" element={<NewPorject />} />
           </Routes>
         </Container>
-        <p>Footer</p>
+        <Footer />
       </Router>
     </div>
   );
