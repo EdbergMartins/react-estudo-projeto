4111a26ad53186b992974f82434f1099f42ebe76
EdberMartins <edbergmartins@gmail.com>
2022-01-12 22:34:46 -0300

    iniciando o home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,17 @@
+import { styles } from "./Home.module.css";
+import savings from "../../img/savings.svg";
+
 function Home() {
-  return(
-    <h1>Home</h1>
-    
-  )
+  return (
+    <section>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+        <p>Comece a gerenciar seus projetos agora mesmo!</p>
+        <a href="/">Criar projeto</a>
+        <img src={savings} alt="Costs" />
+      </h1>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
