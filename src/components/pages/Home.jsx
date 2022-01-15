1afd67f0d036c2502ab57083eb310772254189fc
EdberMartins <edbergmartins@gmail.com>
2022-01-15 17:32:24 -0300

    finalização da pagina home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,16 @@
-import { styles } from "./Home.module.css";
+import styles from "./Home.module.css";
 import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
 
 function Home() {
   return (
-    <section>
+    <section className={styles.home_container}>
       <h1>
         Bem-vindo ao <span>Costs</span>
-        <p>Comece a gerenciar seus projetos agora mesmo!</p>
-        <a href="/">Criar projeto</a>
-        <img src={savings} alt="Costs" />
       </h1>
+      <p>Comece a gerenciar seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Novo Projeto" />
+      <img src={savings} alt="Costs" />
     </section>
   );
 }
