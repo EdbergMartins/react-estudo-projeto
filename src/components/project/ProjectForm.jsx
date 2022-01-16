ba62f4824e8306b2615193886655a703a21dae77
EdberMartins <edbergmartins@gmail.com>
2022-01-16 15:08:19 -0300

    criação do bd e utilização do useEfects para carregar com api

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+
 import styles from "./ProjectForm.module.css";
 import Input from "../form/Input.jsx";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -20,7 +38,11 @@ function ProjectForm({ btnText }) {
           placeholder="Insira o orçamento total do projeto"
         />
       </div>
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
