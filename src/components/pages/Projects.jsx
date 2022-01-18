38ca8b9c074110ea43c0dc71d08b8e210a3ee5c5
EdberMartins <edbergmartins@gmail.com>
2022-01-18 09:07:35 -0300

    Preparação da pagina projetos para interação com api

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,9 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
   const location = useLocation();
@@ -10,9 +14,15 @@ function Projects() {
   console.log({ message });
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Novo Projeto" />
+      </div>
       {message && <Message type="sucess" msg="Procejo criado com sucesso" />}
+      <Container className="start">
+        <p>Projetos....</p>
+      </Container>
     </div>
   );
 }
