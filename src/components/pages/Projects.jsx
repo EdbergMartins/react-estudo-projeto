c5204789911920b30f232bfea2b72017a2bdfef2
EdberMartins <edbergmartins@gmail.com>
2022-01-18 22:33:26 -0300

    excluir projetos, com msg de exclusão

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -11,6 +11,7 @@ import { useEffect, useState } from "react";
 function Projects() {
   const [projects, setProjects] = useState([]);
   const [removeLoading, setRemoveLoading] = useState(false);
+  const [projectMessage, setProjectMessage] = useState();
 
   const location = useLocation();
   let message = "";
@@ -33,6 +34,20 @@ function Projects() {
       .catch((err) => console.log(err));
   }, []);
 
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(projects.filter((project) => project.id !== id));
+        setProjectMessage("Projeto removido com sucesso!");
+      });
+  }
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -40,6 +55,7 @@ function Projects() {
         <LinkButton to="/newproject" text="Novo Projeto" />
       </div>
       {message && <Message type="sucess" msg="Procejo criado com sucesso" />}
+      {projectMessage && <Message type="sucess" msg={projectMessage} />}
       <Container className="start">
         {projects.length > 0 &&
           projects.map((project) => (
@@ -49,10 +65,11 @@ function Projects() {
               budget={project.budget}
               category={project.category.name}
               key={project.id}
+              handleRemove={removeProject}
             />
           ))}
         {!removeLoading && <Load />}
-        {removeLoading && projects.length == 0 && (
+        {removeLoading && projects.length === 0 && (
           <p> Não há projetos cadastrados</p>
         )}
       </Container>
--- src/components/project/ProjectCards.jsx
+++ src/components/project/ProjectCards.jsx
@@ -4,6 +4,11 @@ import styles from "./ProjectCards.module.css";
 import { BsPencil, BsFillTrashFill } from "react-icons/bs";
 
 function ProjectCards({ id, name, budget, category, handleRemove }) {
+  const remove = (e) => {
+    e.preventDefault();
+    handleRemove(id);
+  };
+
   return (
     <div className={styles.project_card}>
       <h4>{name}</h4>
@@ -18,7 +23,7 @@ function ProjectCards({ id, name, budget, category, handleRemove }) {
         <Link to="/">
           <BsPencil /> Editar
         </Link>
-        <button>
+        <button onClick={remove}>
           <BsFillTrashFill /> Excluir
         </button>
       </div>
