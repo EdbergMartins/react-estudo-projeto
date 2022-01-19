14fd5236b6041d064c93141b1d336a48cfa14adf
EdberMartins <edbergmartins@gmail.com>
2022-01-18 22:55:24 -0300

    criação de rota para edição

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Projects from "./components/pages/Projects";
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Project from './components/pages/Project';
 
 function App() {
   return (
@@ -17,10 +18,11 @@ function App() {
         <Container customClass="min-height">
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/company" element={<Company />} />
-            <Route exact path="/contact" element={<Contact />} />
-            <Route exact path="/newproject" element={<NewProject />} />
-            <Route exact path="/project" element={<Projects />} />
+            <Route  path="/company" element={<Company />} />
+            <Route  path="/contact" element={<Contact />} />
+            <Route  path="/newproject" element={<NewProject />} />
+            <Route  path="/project" element={<Projects />} />
+            <Route  path="/project/:id" element={<Project />} />
           </Routes>
         </Container>
         <Footer />
--- src/components/project/ProjectCards.jsx
+++ src/components/project/ProjectCards.jsx
@@ -20,7 +20,7 @@ function ProjectCards({ id, name, budget, category, handleRemove }) {
         {category}
       </p>
       <div className={styles.project_card_actions}>
-        <Link to="/">
+        <Link to={`/project/${id}`}>
           <BsPencil /> Editar
         </Link>
         <button onClick={remove}>
